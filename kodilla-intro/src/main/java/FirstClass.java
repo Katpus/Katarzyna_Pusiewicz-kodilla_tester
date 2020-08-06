74afd87a99b2d6e9709fc52bc8d7e35e04bdd9a9
Katarzyna Pusiewicz <pusiewicz.katarzyna@gmail.com>
2020-08-06 16:06:47 +0200

    Leap Year, modified

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,17 +1,24 @@
 public class FirstClass {
     public static void main(String[] args) {
+        Notebook blacknotebook = new Notebook(700, 800, 2001);
+        Notebook greenNotebook = new Notebook(1100, 500, 2019);
+        Notebook blueNotebook = new Notebook(1600, 1500, 1996);
+        Notebook pinkNotebook = new Notebook ( 7000, 2000, 1995);
+        Notebook yellowNotebook = new Notebook (4000, 3700, 2018);
+        Notebook greyNotebook = new Notebook( 850, 6000, 2014);
 
-        Notebook notebook = new Notebook("700g", 800, 2001);
-        System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
-        notebook.theBestChoice();
-
-        Notebook heavyNotebook = new Notebook("500g", 500, 2019);
-        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price+ " " + heavyNotebook.year);
-        heavyNotebook.theBestChoice();
-
-        Notebook oldNotebook = new Notebook("1600g", 1500, 1996);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + oldNotebook.year);
-        oldNotebook.theBestChoice();
+        System.out.println(blacknotebook.weight + " " + blacknotebook.price + " " + blacknotebook.year);
+        blacknotebook.theBestChoice();
+        System.out.println(greenNotebook.weight + " " + greenNotebook.price+ " " + greenNotebook.year);
+        greenNotebook.theBestChoice();
+        System.out.println(blueNotebook.weight + " " + blueNotebook.price + " " + blueNotebook.year);
+        blueNotebook.theBestChoice();
+        System.out.println(pinkNotebook.weight + " " + pinkNotebook.price + " " + pinkNotebook.year);
+        pinkNotebook.theBestChoice();
+        System.out.println(yellowNotebook.weight + " " + yellowNotebook.price + " " + yellowNotebook.year);
+        yellowNotebook.theBestChoice();
+        System.out.println(greyNotebook.weight + " " + greyNotebook.price + " " + greyNotebook.year);
+        greyNotebook.theBestChoice();
 
     }
 }
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,21 +1,17 @@
 public class Notebook {
-    String weight;
+    int weight;
     int price;
     int year;
 
-    public Notebook(String weight, int price, int year) {
+    public Notebook(int weight, int price, int year) {
         this.weight = weight;
         this.price = price;
         this.year = year;
     }
 
     public void theBestChoice() {
-        if (this.price < 600 && this.year > 2015) {
+        if (this.price < 2000 && this.year > 2015 && this.weight < 1200) {
             System.out.println("The best choice ever");
-        } else if (this.price > 700  && this.year > 2000) {
-            System.out.println("Could be better.");
-        } else if (this.price > 1000 && this.year < 2000) {
-            System.out.println("Please, don't buy it.");
         }
     }
 }
